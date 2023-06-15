2c509984c0dfb453e8bd203df734e441b56bfe96
Tanatswa Gendere <tanagends@gmail.com>
2023-06-15 15:36:58 -0700

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using printf
- * Return: 0 (Success)
- */ 
+ *main - A program that prints a line using printf
+ *Return: 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
