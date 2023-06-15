6edc9d0f2ccab41694be4ddd01a5392a6ecc6c80
Tanatswa Gendere <tanagends@gmail.com>
2023-06-15 15:29:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main: a program printing formatted standard output
+ * Return: 0 (Success)
+ */ 
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
