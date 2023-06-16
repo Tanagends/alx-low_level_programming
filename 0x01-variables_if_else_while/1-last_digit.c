75eb43f46f11979eec4ec5289c54c0d28066a12b
Tanatswa Gendere <tanagends@gmail.com>
2023-06-16 11:32:54 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,5 +17,5 @@ int main(void)
 		printf("Last digit of %c is %d and is 0\n", 'n', n % 10);
 	else
 		printf("Last digit of %c is %d and is less than 6 and not 0\n", 'n', n % 10);
-	return (0)
+	return (0);
 }
