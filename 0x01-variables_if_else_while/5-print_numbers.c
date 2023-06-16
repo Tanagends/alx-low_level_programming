9eb8a04bd7819223ac52f2f7f4a0ec5c174c62de
Tanatswa Gendere <tanagends@gmail.com>
2023-06-16 10:59:06 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints numbers.
+ * main - prints numbers
  * Return: 0(Success)
  */
 int main(void)
 {
-	int s;
-
-	for (s = 0; s < 10; s++)
-		printf("%d\n", s);
+	printf("0123456789\n");
 	return (0);
 }
-	
