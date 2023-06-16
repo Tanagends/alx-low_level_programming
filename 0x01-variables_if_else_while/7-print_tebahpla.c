71af6591e8b735d918a59345d17c7e41fca00323
Tanatswa Gendere <tanagends@gmail.com>
2023-06-16 12:33:53 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - outputs capital alphabet letters in reverse.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char s;
+
+	for (s = 'Z'; s >= 'A'; s--)
+		putchar(s);
+	putchar('\n');
+	return (0);
+}
