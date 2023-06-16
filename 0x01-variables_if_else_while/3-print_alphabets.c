f2eb74fa2d027092700440a35c687cd5a0b564df
Tanatswa Gendere <tanagends@gmail.com>
2023-06-16 12:12:53 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lower case.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+	putchar(c)
+for (c = 'A'; c <= 'Z'; c++)
+	putchar(c);
+putchar('\n');
+return (0);
+}
