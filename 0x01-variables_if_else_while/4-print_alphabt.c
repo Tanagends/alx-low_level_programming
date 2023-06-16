6de98d0077bd1a4595f46936c7e3f2945562f313
Tanatswa Gendere <tanagends@gmail.com>
2023-06-16 09:59:18 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet characters.
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	for (char = a; char <= z; char++)
+		if (char == q || char == e)
+			continue;
+		putchar(char);
+	return (0);
+}
