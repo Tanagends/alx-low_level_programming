a5460a23dc7d899d8eedd508ddaab2275a400e04
Tanatswa Gendere <tanagends@gmail.com>
2023-06-20 02:19:54 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar, followed by a new line.
+ * Return: (0) Success.
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Functions and nested loops tasks.
+Each file is closely related tp its file name.
