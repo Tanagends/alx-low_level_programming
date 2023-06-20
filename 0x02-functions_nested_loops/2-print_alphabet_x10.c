50711a805788e7c70f9f6f5d2b1220a9b71b8ff6
Tanatswa Gendere <tanagends@gmail.com>
2023-06-20 15:32:59 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/** 
+ * print_alphabet_x10 - prints alphabet 10 times.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 9; i++)
+	{	for (c = 'a'; c = 'z'; c++)
+		       putchar(c);
+	}
