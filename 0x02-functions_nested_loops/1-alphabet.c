d44b44f1d9a247d42dbd92bb567c2d376b07d38f
Tanatswa Gendere <tanagends@gmail.com>
2023-06-20 13:30:20 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,13 +7,5 @@
 int main(void)
 {
 	print_alphabet();
-	putchar('\n');
 	return (0);
-}
-void print_alphabet(void)
-{
-	char s;
-
-	for (s = 'a';s <= 'z'; s++)
-		putchar(s);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,8 @@
 void print_alphabet(void);
+{
+	char s;
+
+	for (s='a'; s<='z'; s++)
+		putchar(s);
+	putchar(s);
+}
