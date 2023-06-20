09b6d294c9dbce7111d42d65a7389014d1d46352
Tanatswa Gendere <tanagends@gmail.com>
2023-06-20 14:57:23 -0700

    1-main.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+/* print_alphabet - prints alphabet.*/
 void print_alphabet(void)
 {
 	char s;
 
-	for (s='a'; s<='z'; s++)
+	for (s = 'a'; s <= 'z'; s++)
 		putchar(s);
 	putchar('\n');
 }
