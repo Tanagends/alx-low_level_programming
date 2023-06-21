a3c88f7ae83d46ad0785c1324b7bc1794a7a5d18
Tanatswa Gendere <tanagends@gmail.com>
2023-06-21 07:39:45 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _abs - finds the absolute value of an int.
+ * @int: - integer to be computed into its absolute value
+ * Return: int absolute value.
+ */
+int _abs(int)
+{
+	int c;
+
+	if (c < 0)
+		return (-c);
+	return (c);
