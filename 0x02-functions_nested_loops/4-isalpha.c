aa9c900b28caab4a41502c53610888ee86d7ef13
Tanatswa Gendere <tanagends@gmail.com>
2023-06-20 17:20:01 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+/**
+ * _isalpha - checks is c is a character.
+ * Return: 1(Success)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return 1;
+	else 
+		return 0;
+}
