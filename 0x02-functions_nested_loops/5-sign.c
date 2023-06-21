597e92a1ba91e1f648ca6338f458fd753bc9ba92
Tanatswa Gendere <tanagends@gmail.com>
2023-06-21 06:54:24 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,16 +10,22 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
+		putchar('1');
+		putchar('\n');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		putchar('0');
+		putchar('0');
+		putchar('\n');
 		return (0);
 	}
 	else
 	{
 		putchar('-');
+		putchar('1');
+		putchar('\n');
 		return (-1);
 	}
 }
