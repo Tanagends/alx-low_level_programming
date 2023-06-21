1870cb721540a24bf6dc8ee175bfb094bf7a88c7
Tanatswa Gendere <tanagends@gmail.com>
2023-06-20 17:42:09 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 /**
- * print_sign - prints sign.
+ * print_sign - prints sign of n.
  * Return:  1 or -1 or 0 (success)
  */
 
 int print_sign(int n)
 {
-    if (n > 0) {
-        putchar('+');
-        return 1;
-    } else if (n == 0) {
-        putchar('0');
-        return 0;
-    } else {
-        putchar('-');
-        return -1;
-    }
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
 }
