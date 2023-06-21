357266e2c2accc1c6b9e7e6809f23f59b7679420
Tanatswa Gendere <tanagends@gmail.com>
2023-06-21 12:47:30 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* add - adds two integers.
+* @e: int to be added to f
+* @f: int to be added to e
+* Return: sum of e and f
+*/
+int add(int e, int f)
+{
+	int g;
+
+	g = e + f;
+	return (g);
+}
