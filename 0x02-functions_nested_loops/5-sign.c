82bc174f4d07bbac8015030609a114f180858670
Tanatswa Gendere <tanagends@gmail.com>
2023-06-21 11:12:51 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,22 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
-		putchar('1');
+		putchar(43);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar('0');
-		putchar('0');
-		putchar('\n');
+		_putchar(48);
 		return (0);
 	}
 	else
 	{
-		putchar('-');
-		putchar('1');
-		putchar('\n');
+		putchar(45);
 		return (-1);
 	}
 }
