06316796757e63ab2f917a938ff9e0cf54fd79cd
Tanatswa Gendere <tanagends@gmail.com>
2023-06-22 12:45:16 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_square - print a square.
+ *@size: one dimension of square.
+ */
+void print_square(int size)
+{
+	int i;
+	int k;
+	int y;
+
+	if (size <= 0)
+		putchar('\n');
+	for (i = 0; i < size; i++)
+		putchar('#');
+	putchar('\n');
+	for (k = 0; k < size - 2; k++)
+	{
+		putchar('#');
+		for (y = 0; y < size - 2; y++)
+			putchar(' ');
+		putchar('#');
+		putchar('\n');
+	}
+	for (i = 0; i < n; i++)
+		putchar('#');
+	putchar('\n');
+}
