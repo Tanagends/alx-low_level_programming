609a8b12ee0ed8e3748264721e34d0f1402fd2fb
Tanatswa Gendere <tanagends@gmail.com>
2023-06-22 11:45:46 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,9 +7,13 @@
 void print_diagonal(int n)
 {
 	int i;
+	int k;
 
 	for (i = 0; i < n; i++)
+	{
+		for (k = 1; k < n; k++)
+			_putchar(32);
 		_putchar(92);
-		_putchar(32);
 	_putchar(10);
+	}
 }
