78c823c274e832168b802996534342e9ce1916fa
Tanatswa Gendere <tanagends@gmail.com>
2023-06-22 06:04:33 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: c (product).
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
