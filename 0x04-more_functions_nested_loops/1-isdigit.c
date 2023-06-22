52ecd4e7f4e3460296d1217927cb5456a249cf4c
Tanatswa Gendere <tanagends@gmail.com>
2023-06-22 05:27:54 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *_isdigit - checks for a digit.
+ * @c: the parameter to be checked.
+ * Return: 1 (success)
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c < 10)
+		return (0);
+	else
+		return (0);
+}
