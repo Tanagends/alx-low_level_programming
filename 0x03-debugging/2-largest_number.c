c55387e648193d7356cab1b5b8a4a3efd94a3e0d
Tanatswa Gendere <tanagends@gmail.com>
2023-06-22 16:30:50 -0700

    1-main.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,17 +13,13 @@ int largest_number(int a, int b, int c)
 
 int largest;
 
-if (a > b && a > c)
+if (a >= b && a >= c)
 {
 	largest = a;
 }
-else if (a > b && c > a)
+else if (a >= b && c >= a)
 {
 	largest = c;
-}
-else if (b > c)
-{
-	largest = b;
 }
 else
 {
