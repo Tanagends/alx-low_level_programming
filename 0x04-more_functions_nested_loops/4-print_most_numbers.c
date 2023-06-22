31a725d3eefad01e8dd29a51e1c18f842e11bffd
Tanatswa Gendere <tanagends@gmail.com>
2023-06-22 06:27:00 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * print_numbers - prints 0-9
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		if (c == 2 || c == 4)
+			continue;
+		putchar(c + '0');
+	putchar('\n');
+}
