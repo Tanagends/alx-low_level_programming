56f933b1bb17138296a1010b955e2ea55babf0ab
Tanatswa Gendere <tanagends@gmail.com>
2023-06-22 07:02:10 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+/**
+ * more_numbers - multiplies 0-14 by 10.
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 0; a < 15; a++)
+		{
+			if (a < 10)
+				putchar(a + '0');
+			else if (a > 9)
+			{
+				putchar(a / 10 + '0');
+				putchar(a % 10 + '0');
+			}
+		}
+	}
+}
