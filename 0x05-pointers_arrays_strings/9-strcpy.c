6676a227529a7a2618c9919ac13e2849431a74eb
Tanatswa Gendere <tanagends@gmail.com>
2023-06-26 10:11:27 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - prints a string.
+ * @src: array to be copied.
+ * @dest: array to be copied into..
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
