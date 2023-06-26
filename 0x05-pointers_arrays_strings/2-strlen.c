f824bd90f815e2b6ca16d1f1ff2dda3b04bb4d76
Tanatswa Gendere <tanagends@gmail.com>
2023-06-26 03:55:39 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+/**
+ * _strlen - returns the length of a string.
+ * @s: pointer to a string literal.
+ * Return: 0 (Always a success).
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	l = strlen(s);
+	return (l);
+}
