ba7790d139bcf4d2d71fc68b0ffc957aa41124b9
Tanatswa Gendere <tanagends@gmail.com>
2023-06-26 04:42:21 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - prints a string.
+ * @s: the string pointer.
+ */
+void print_rev(char *s)
+{
+	int i;
+	int k = strlen(s);
+
+	for (i = k - 1; i > -1; i--)
+		putchar(str[i]);
+	putchar('\n');
+}
