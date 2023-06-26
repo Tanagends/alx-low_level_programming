b7e58f11860c3863653dbe57ef27b4020bb79070
Tanatswa Gendere <tanagends@gmail.com>
2023-06-26 09:51:07 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,9 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-		putchar(*a[i]);
-		if (i != n - 1)
-			putchar(',');
-			putchar(' ');
-	putchar('\n');
+	{	if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
+	}
 }
