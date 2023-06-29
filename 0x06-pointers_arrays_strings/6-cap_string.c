99b1aea834dce7587b4da87887e3f0a2684fae15
Tanatswa Gendere <tanagends@gmail.com>
2023-06-29 14:25:30 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+/**
+ * cap_string - converts string to upper case.
+ * @s: string to be converted.
+ * Return: string in upper.
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
