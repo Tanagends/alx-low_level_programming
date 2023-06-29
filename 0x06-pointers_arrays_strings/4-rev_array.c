fac8f8a4ac30d095edb52227b9e0880e207fc009
Tanatswa Gendere <tanagends@gmail.com>
2023-06-29 13:34:06 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+/**
+ * reverse_array - reverses array
+ * @a: array.
+ * @n: number of array elements.
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		a[i] = a[n - 1 - i);
+		a[n - 1 - i) = a[i];
+	}
+}
