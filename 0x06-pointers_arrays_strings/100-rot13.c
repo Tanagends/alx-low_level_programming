9efcd240cf9fefca2cae366674b3669ba59a281b
Tanatswa Gendere <tanagends@gmail.com>
2023-06-29 16:10:45 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,1 +1,22 @@
-#
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+char *rot13(char *str) {
+    int i, j;
+    char *rot13Str = str;
+
+    for (i = 0; i < strlen(str); i++) {
+        if (isalpha(str[i])) {
+            for (j = 0; j < 13; j++) {
+                if ((str[i] == 'z' && j < 13) || (str[i] == 'Z' && j < 13)) {
+                    rot13Str[i] -= 13;
+                    break;
+                }
+                rot13Str[i]++;
+            }
+        }
+    }
+
+    return rot13Str;
+}
