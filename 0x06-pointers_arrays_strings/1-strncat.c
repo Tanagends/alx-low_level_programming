6f4aa95035bbfc55dd604ca0a2a5222b86141d45
Tanatswa Gendere <tanagends@gmail.com>
2023-06-29 09:17:32 -0700

    1-

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,24 @@
 /**
- * _strcat - concatenates two strings.
+ * _strncat - concatenates two strings.
  * @dest: pointer to the first string.
  * @src: pointer to the second string.
+ * @n: maximum length of src to be concatenated.
  * Return: dest(the resulting string).
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	char *init = dest;
-	int n
+	int i = 0;
 
 	while (*init != '\0')
 		*init++;
-	while (*src != '\0')
+	while (i++ < n)
 	{
 		*init = *src;
 		init++;
 		src++;
 	}
-	*init = '\0';
+	if (*src == '\0')
+		*init = '\0';
 	return (dest);
 }
