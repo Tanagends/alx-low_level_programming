cae64fa44188d6d6d4480ee916335ec621c91dee
Tanatswa Gendere <tanagends@gmail.com>
2023-06-29 13:22:20 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,12 @@ int _strcmp(char *s1, char *s2)
 	int i;
 
 	i = 0;
-	while ((s1[i] == s2[i] && (s1[i] != '\0' && s2[i] != '\0'))
+	while (s1[i] == s2[i] && (s1[i] != '\0' && s2[i] != '\0'))
 		i++;
 	if (s1[i] > s2[i] || s2[i] == '\0')
-		return (1);
+		return (1 * (s1[i] - s2[i]));
 	else if (s1[i] < s2[i] || s1[i] == '\0')
-		return (-1);
-	else if (s1[i] == '\0' && s2[i] == '\0')
+		return (-1 * (s2[i] - s1[i]));
+	else
 		return (0);
 }
