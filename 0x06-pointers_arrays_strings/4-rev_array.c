b0dd482c23f34dbb5b8e7c5e98d162aae385f2b8
Tanatswa Gendere <tanagends@gmail.com>
2023-06-29 13:47:49 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,6 +6,7 @@
 void reverse_array(int *a, int n)
 {
 	int i = 0;
+	char temp;
 
 	for (i = 0; i < n / 2; i++)
 	{
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: pointer to the first string.
+ * @src: pointer to the second string.
+ * Return: dest(the resulting string).
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *init = dest;
+
+	while (*init != '\0')
+		*init++;
+	while (*src != '\0')
+	{
+		*init = *src;
+		init++;
+		src++;
+	}
+	*init = '\0';
+	return (dest);
+}
