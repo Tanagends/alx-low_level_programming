d66fa48a642c4d7acfcbe8a744ad0599392b8c30
Tanatswa Gendere <tanagends@gmail.com>
2023-07-03 14:34:41 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - prints the sum of two diagonals 
+ * print_diagsums - prints the sum of two diagonals
  * @a: input
  * @size: input
  * Return: Always 0 (Success)
