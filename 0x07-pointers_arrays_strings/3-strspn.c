b988a0d1c6fbde7bf6a34cd34623d06aadfcb8a3
Tanatswa Gendere <tanagends@gmail.com>
2023-07-03 06:42:13 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,11 +7,10 @@
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int i;
-unsigned int l;
-unsigned int k, j;
+unsigned int j;
+unsigned int k;
 
-j = 1;
-outer_loop:
+j = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
 	for (k = 0; accept[k] != '\0'; k++)
@@ -20,7 +19,7 @@ for (i = 0; s[i] != '\0'; i++)
 		{
 			j++;
 			/*continue to the outer loop*/
-			continue outer_loop;
+			break;
 		}
 	}
 }
