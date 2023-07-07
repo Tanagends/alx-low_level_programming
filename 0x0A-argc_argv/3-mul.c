a7860e10f8014c33185acd7a5b3056864a84b095
Tanatswa Gendere <tanagends@gmail.com>
2023-07-07 07:51:57 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 /**
  * main - prints multiplication result.
@@ -10,10 +11,14 @@ int main(int argc, char *argv[])
 	int product;
 
 	if (argc != 3)
+	{
 		printf("Error\n");
 		return (-1);
+	}
 	else
-		product = atoi(arg[1]) * atoi(argv[2]);
+	{
+		product = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", product);
+	}
 	return (0);
 }
