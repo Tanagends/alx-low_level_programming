95a83db8fe01299c5765c6f94529df2d89587986
Tanatswa Gendere <tanagends@gmail.com>
2023-07-07 07:31:25 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints argument count.
+ * @argc: argument count.
+ * @argv: command line arguments.
+ * Return: Always 0(success).
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
