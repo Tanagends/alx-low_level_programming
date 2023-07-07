30420337674cdb00d32d47759d5e49e18737ffa0
Tanatswa Gendere <tanagends@gmail.com>
2023-07-07 07:08:42 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+/**
+ * main - prints its own name.
+ * @argc: argument count.
+ * @argv: command line arguments.
+ * Return: Always 0(success).
+ */
+int main(int argc, char *argv[])
+{
+	printf("%S\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+These are tasks.
+The topic is command line arguments.
