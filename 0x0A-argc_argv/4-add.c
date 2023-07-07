68e79e65ee278960356024824cfdfe3928de9f7b
Tanatswa Gendere <tanagends@gmail.com>
2023-07-07 08:43:52 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds command line argument numbers.
+ * @argc: argument count.
+ * @argv: command line arguments.
+ * Return: 0(success).
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i = 1;
+
+	if (argc == 1)
+		printf("0\n");
+	
+	for (; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0 && **(argv + i) != '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
