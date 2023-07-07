1527b703c48de8560bbd1fd063902c1e0fa2e07d
Tanatswa Gendere <tanagends@gmail.com>
2023-07-07 16:27:14 -0700

    0

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Compile all the .c files in the current directory into .o files
 gcc -Wall -Werror -Wextra -pedantic -c *.c
 
+# Create the static library from the .o files
 ar -rc liball.a *.o
 
+# Index the symbols in the library for faster linking
 ranlib liball.a
 
+# Remove the .o files
 rm *.o
