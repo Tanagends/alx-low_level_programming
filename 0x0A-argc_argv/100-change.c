29cc67b48d56603a5fc68808043e07fefd9d8791
Tanatswa Gendere <tanagends@gmail.com>
2023-07-07 13:58:16 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * main - prints multiplication result.
+ * main - prints the minimum number of coins in change.
  * @argc: argument count.
  * @argv: command line arguments.
  * Return: Always 0(success).
@@ -10,12 +10,13 @@ int main(int argc, char *argv[])
 {
 	int num = 0;
 	int divnum = 0;
-	int cents = atoi(argv[1]);
+	int cents;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
+	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
