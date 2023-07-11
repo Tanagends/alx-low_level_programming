ab5093771e8e017bc8670741609d16d7916dfaf4
Tanatswa Gendere <tanagends@gmail.com>
2023-07-11 06:51:51 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars and initializes with c.
+ * @size: array size.
+ * @c: the char.
+ * Return: pointer to the array.
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	int i;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(size * (sizeof(char)));
+	if (!array)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return (array);
+}
