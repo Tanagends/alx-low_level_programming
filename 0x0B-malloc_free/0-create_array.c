d1be533ce924fc0788cc9e7c31cc9c637a5c6f4c
Tanatswa Gendere <tanagends@gmail.com>
2023-07-11 05:19:05 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars and initializes with c.
+ * @size: array size.
+ * @c: the char.
+ * Return: pointer to the array.
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+
+	if (size = 0)
+		return (NULL);
+	array = malloc(size * (sizeof(unsigned int));
+	if (!array)
+		return (NULL);
+	array[0] = c;
+	return (array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+Hi 
+These are malloc assignments.
