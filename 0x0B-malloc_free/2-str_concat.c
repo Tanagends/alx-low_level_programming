9a8786393180a95c5bc364983448ee37497dced0
Tanatswa Gendere <tanagends@gmail.com>
2023-07-11 12:56:42 -0700

    0-create_array.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,9 +20,9 @@ char *str_concat(char *s1, char *s2)
 	array = malloc((i + k + 1) * sizeof(char));
 	if (!array)
 		return (NULL);
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	strcpy(array, s1);
-	strcat(array, s2);
+	if (s1 == NULL)
+		strcpy(array, s1);
+	if (s2 == NULL)
+		strcat(array, s2);
 	return (array);
 }
