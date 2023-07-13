33f6c60315e4d3d55fd13774af5aed3fcd32f5a5
Tanatswa Gendere <tanagends@gmail.com>
2023-07-13 10:48:00 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,15 +8,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *a;
+	unsigned int b = 0;
+	unsigned int len;
 
 	if (nmemb == 0 || size == 0)
 		exit(0);
-	a = malloc(size * nmemb);
+	len = size * nmemb;
+	a = malloc(len);
 	if (a == NULL)
 		exit(0);
-	memset(a, 0, size * nmemb);
+	for (b = 0; b < len; b++)
+		a[b] = 0;
 	return (a);
 }
-/**
- * memset - 
- *
