1a427f8016bb1eaa30ea6fa110ea07c6ce31c3a1
Tanatswa Gendere <tanagends@gmail.com>
2023-07-13 11:31:58 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,20 @@
 #include <stdlib.h>
 /**
+ * memset - sets memory.
+ * @c: char to be set.
+ * @p: string to be set.
+ * @len: setting length.
+ * Return: set string.
+ */
+char *memsett(char *p, char c, unsigned int len)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < len; i++)
+		p[i] = c;
+	return (p);
+}
+/**
  * _calloc - allocates memory.
  * @nmemb: element count.
  * @size: element size.
@@ -7,7 +22,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *a;
+	char *a;
 	unsigned int b = 0;
 	unsigned int len;
 
@@ -17,7 +32,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	a = malloc(len);
 	if (a == NULL)
 		exit(0);
-	for (b = 0; b < len; b++)
-		a[b] = 0;
+	memsett(a, 0, len);
 	return (a);
 }
