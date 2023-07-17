29ef02bc2e14a2307b8103da45d6165c6f3035eb
Tanatswa Gendere <tanagends@gmail.com>
2023-07-17 13:29:34 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog - dog profile.
+ * @name: the dog's name.
+ * @age: the dog's age.
+ * @owner: the dog's owner.
+ * @d: struct.
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
