1b906cc6a1ea78a214d9a4944e1f19d03c5e6700
Tanatswa Gendere <tanagends@gmail.com>
2023-07-19 15:47:32 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,29 +1,43 @@
+#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
 /**
- * main - operates on two numbers.
- * @argc: command line argument count.
- * @argv: argument input.
- * Return: 0 (Success).
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int _attribute((__unused_)) argc, char *argv[])
 {
-	int i, t;
-	char *c = argv[2];
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
 		exit(99);
 	}
-	i = atoi(argv[1]);
-	t = atoi(argv[3]);
-	if (get_op_func(c) == NULL || *c == '\0')
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(c)(i, t));
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
