fe5ed45eea35031f185e3981dbb0f1fbe28e288b
Tanatswa Gendere <tanagends@gmail.com>
2023-07-19 13:25:59 -0700

    0-

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - searchesfor an integer.
+ * @array: the array.
+ * @size: array size.
+ * @cmp: function which compares values.
+ * @Return: index of first similar element.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	while (!array(i) && i < size)
+		i++;
+	if (i == size)
+		return (-1);
+	return (i);
+}
