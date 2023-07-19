d6a58d2c49b686c5c006fc4aeb9fc6d239ea8973
Tanatswa Gendere <tanagends@gmail.com>
2023-07-19 13:44:20 -0700

    0-

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,6 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
+	if (array == NULL || cmp == NULL)
+		return (-1);
 	while (cmp(array[i]) == 0 && i < size)
 		i++;
 	if (i == size)
