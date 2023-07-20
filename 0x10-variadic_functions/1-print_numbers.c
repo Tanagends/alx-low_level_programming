eb7aca5d630108993ccd789c1f0127ab3f4091aa
Tanatswa Gendere <tanagends@gmail.com>
2023-07-20 12:38:24 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * print_numbers - prints all input integers.
  * @n: number of integers.
@@ -14,9 +15,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n - 1; i++)
 	{
 		printf("%d", va_arg(intPrint, int));
-		if (separator != '\0')
+		if (separator != NULL)
 			printf("%s", separator);
 	}
-	printf("%d\n", va_arg(intPrint, int);
+	printf("%d\n", va_arg(intPrint, int));
 	va_end(intPrint);
 }
