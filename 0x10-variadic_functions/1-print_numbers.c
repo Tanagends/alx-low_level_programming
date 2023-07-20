af461cd6395a81d0a8fb1cbfc80b5ca03719ceef
Tanatswa Gendere <tanagends@gmail.com>
2023-07-20 12:42:31 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,6 +11,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list intPrint;
 	unsigned int i = 0;
 
+	if (n == 0)
+		return;
 	va_start(intPrint, n);
 	for (i = 0; i < n - 1; i++)
 	{
