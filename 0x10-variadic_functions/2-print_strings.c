75f5fe471a6995389d4f69b8f3e74a6040ddea9c
Tanatswa Gendere <tanagends@gmail.com>
2023-07-20 13:37:34 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", str);
 		if (separator != NULL && i < n - 1)
-			printf("%s", seprator);
+			printf("%s", separator);
 	}
 	va_end(intPrint);
 	printf("\n");
