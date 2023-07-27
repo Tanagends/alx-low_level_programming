9910a6bf818dcc015c052993ef4f17ecf1589c6b
Tanagends <tanagends@gmail.com>
2023-07-27 14:17:05 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
 #ifndef MACRO_H
 #define MACRO_H
+#include <stddef.h>
+#include <stdio.h>
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node_end(list_t **head, const char *str);
