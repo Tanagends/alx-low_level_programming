7fbabca8a9de08c6a4258a7af084ab2167c9a1a4
Tanagends <tanagends@gmail.com>
2023-07-27 14:52:53 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@ size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
@@ -17,9 +17,5 @@ size_t print_list(const list_t *h)
 		i++;
 		h = h->next;
 	}
-	if (h->str == NULL)
-		printf("[%d] %s\n", 0, "(nil)");
-	else
-		printf("[%d] %s\n", h->len, h->str);
-	return (++i);
+	return (i);
 }
