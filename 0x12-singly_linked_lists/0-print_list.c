d938fe2de0485c22d846b51939857f1e2ac21b2a
Tanagends <tanagends@gmail.com>
2023-07-27 14:03:43 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,16 @@
+/**
+ * print_list - prints elements of a list.
+ * @h: initial node.
+ * Return: node count.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h->next != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+These are my tasks on single lists.
+My first data structure after arrays.
