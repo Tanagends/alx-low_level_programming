a746f5a3b2fd76c62e3cf84a07ac03cedd31697e
Tanagends <tanagends@gmail.com>
2023-07-27 18:04:47 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,9 +2,10 @@
 /**
  * before_main - executes before main.
  */
-void before_main() __attribute__((constructor));
+void before_main(void) __attribute__((constructor));
 
-void before_main()
+void before_main(void)
 {
-	 printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n
+		I bore my house upon my back!\n");
 }
