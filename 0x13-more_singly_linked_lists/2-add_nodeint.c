bae3915658a552ce7798cb5c5337049a9ca580ea
Tanagends <tanagends@gmail.com>
2023-07-31 10:41:32 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * add_nodeint - adds listint_t node at list start.
- * @h: pointer to head.
+ * @head: pointer to head.
  * @n: int data at node.
  *
  * Return: address of the new element.
