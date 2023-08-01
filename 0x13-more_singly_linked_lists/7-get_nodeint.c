8960c4af1af29fe82b5d9c9f22f69c156b8e11ec
Tanagends <tanagends@gmail.com>
2023-08-01 05:57:53 -0700

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - indexes listint_t list.
+ * @head: pointer to head.
+ * @index: index.
+ * Return: the node index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr = head;
+	unsigned int i = 0;
+
+	for (i = 0; i < index + 1; i++)
+		ptr = ptr->next;
+	return (ptr);
+}
