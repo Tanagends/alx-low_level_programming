f78ccd52875fe791255312ff5e6fe9a1e2ceb633
Tanagends <tanagends@gmail.com>
2023-08-03 14:34:49 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * get_bit - gets decimal's binary index value.
+ * @n: decimal.
+ * @index: position.
+ * return: index value.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+	size_t i;
+
+	if (index >= sizeof(unsigned long int))
+		return (-1);
+
+	mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+
+	for (i = 0; i < index; i++)
+		mask >>= 1;
+	if ((n & mask) != 0)
+		return (1);
+	else
+		return (0);
+}
