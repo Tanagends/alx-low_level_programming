7f1baf42e268366b462670e239eaa52af2f31755
Tanagends <tanagends@gmail.com>
2023-08-03 15:22:08 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * flip_bits - counts bits flip from one decimal to the next..
+ * @n: first decimal.
+ * @m: second decimal.
+ * Return: flip bits count.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int flip, mask, count = 0;
+
+	if (m >= n)
+		flip = m - n;
+	else
+		flip = n - m;
+
+	mask = 1UL << sizeof(unsigned long int * 8 - 1);
+	while (mask)
+	{
+		if (mask & flip != 0)
+			count++;
+		mask >>= 1;
+	}
+	return (count);
+}
