57d55355bdb6911e54f4aa980be9f84d9020dd9a
Tanagends <tanagends@gmail.com>
2023-08-03 16:07:22 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - gets computer memory's endianness.
+ * Return: 1 (Little endianness) or 0 (Big endianness).
+ */
+int get_endianness(void)
+{
+	unsigned long char *ptr;
+	unsigned long int hex = 0x1234;
+
+	ptr = &hex;
+	if (*ptr > *(ptr++))
+		return (1);
+	else
+		return (0);
+}
