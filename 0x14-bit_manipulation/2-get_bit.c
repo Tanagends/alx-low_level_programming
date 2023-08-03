fbde0bcaf53099309a64f22f579911815df448a2
Tanagends <tanagends@gmail.com>
2023-08-03 14:40:50 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,13 +11,11 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int mask;
 	size_t i;
 
-	if (index >= sizeof(unsigned long int))
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	mask = 1UL << index;
 
-	for (i = 0; i < index; i++)
-		mask >>= 1;
 	if ((n & mask) != 0)
 		return (1);
 	else
