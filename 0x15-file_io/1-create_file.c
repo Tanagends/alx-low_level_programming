05cb1d05707ec9f8a051b0f0ad089561836f105f
Tanagends <tanagends@gmail.com>
2023-08-07 15:50:46 -0700

    0-read_textfile.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,11 +14,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file_descriptor = open(filename, O_CREAT, 0600);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_descriptor == -1)
 		return (-1);
 	if (text_content == NULL)
+	{
+		close(file_descriptor);
 		return (1);
+	}
 	for (count = 0; text_content[count]; count++)
 		;
 	writecount = write(file_descriptor, text_content, count + 1);
