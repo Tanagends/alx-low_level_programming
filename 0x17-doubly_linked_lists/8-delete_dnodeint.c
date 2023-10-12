d9717392e98a9790ddb256153a956a8365a4699c
Tanagends <tanagends@gmail.com>
2023-10-13 00:48:57 +0200

    doubly linked lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,22 +8,28 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *copy, *tmp;
+	dlistint_t *copy, *tmp, *before = NULL;
 	unsigned int i;
 
-	if (!head)
+	if (!head || !(*head))
 		return (-1);
 	copy = *head;
 	for (i = 0; i < index; i++)
 	{
 		if (copy)
+		{
+			before = copy;
 			copy = copy->next;
+		}
 		else
 			return (-1);
 	}
+	if (!copy)
+		return (-1);
 	tmp = copy;
 	copy = copy->next;
-	copy->prev = tmp->prev;
+	if (before)
+		before->next = tmp->prev;
 	free(tmp);
 	return (1);
 }
