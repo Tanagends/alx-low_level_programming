ebbfa7bc1b058e2de73c57348538a7df97765aa5
Tanagends <tanagends@gmail.com>
2023-10-13 01:10:31 +0200

    doubly linked lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -34,6 +34,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		copy = copy->next;
 		copy->prev = before;
 	}
+	if (index == 0)
+		*head = copy;
 	free(tmp);
 	return (1);
 }
