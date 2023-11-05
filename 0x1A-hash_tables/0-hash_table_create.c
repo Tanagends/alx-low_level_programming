c2e89eaa200ae2597f98c509033fb3f831f7d815
Tanagends <tanagends@gmail.com>
2023-11-05 23:20:16 +0200

    hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,16 +13,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
-		return NULL;
+		return (NULL);
 
 	new = malloc(sizeof(new_array));
 	if (!new)
-		return NULL;
+		return (NULL);
 
 	memset((*new), 0, sizeof(*new));
-	
+
 	new_table->size = size;
 	new_table->array = new;
 
-	return new_table;
+	return (new_table);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef HASH_H
+#define HASH_H
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -8,9 +11,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -23,8 +26,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
@@ -34,3 +37,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
+#endif
