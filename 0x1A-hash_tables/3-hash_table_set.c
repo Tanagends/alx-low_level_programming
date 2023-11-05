2f555394e43f285d3cda3ef9d2a4d06a55201809
Tanagends <tanagends@gmail.com>
2023-11-06 01:17:43 +0200

    hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,18 +14,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int i;
 	hash_node_t *element;
-	char *key_cpy = "", *value_cpy = "";
+	/*char *key_cpy = "", *value_cpy = "";*/
 
 	if (!key)
 		return (0);
-	strcat(key_cpy, key);
-	strcat(value_cpy, value);
 	element = malloc(sizeof(hash_node_t));
 	if (!element)
 		return (0);
 
-	element->key = key_cpy;
-	element->value = value_cpy;
+	element->key = (char *)key;
+	element->value = (char *)value;
 
 	i = key_index((const unsigned char *)key, ht->size);
 
